8a75f0147a6008ed14b356109e23b0c63d695089
LewisCollum <lewiscollum4@gmail.com>
2022-05-25 15:56:46 -0400

    Set up Poetry, `poetry run` to start application

--- networkcameras/app.py
+++ networkcameras/app.py
@@ -72,6 +72,9 @@ def reboot():
 def index():    
     return render_template("index.html")
 
+
+def main():
+    socketio.run(app, host="0.0.0.0", port=5000)
     
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5001)
+    main()
